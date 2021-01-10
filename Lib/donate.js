796debc704b30d0045b97b816d87819dbf5f7262
17282827 <77242040+17282827@users.noreply.github.com>
2021-01-10 15:04:25 -0300

    Add files via upload

---
+++ Lib/donate.js
@@ -0,0 +1,39 @@
+exports.donate = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT AKTIF: *${aktif}*
+
+         
+╔════════════════════
+║ *DONASI KE ${A187}*
+╠════════════════════
+║╭──❉ *DONASI BOS* ❉──
+║│➸ *OVO*: 0
+║│➸ *PULSA*: 0
+║│➸ *GOPAY*: 0
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Masuk sini😙*
+║│ _${46 933005154}_
+║│➸ *YouTube mimin*
+║│ _${nomer}_
+║│➸ *Instagram <Follow>*
+║│ _${instagram}_
+║│➸ *Creator ${A187}*
+║│ _${MAYK PEZZINI}_
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*POWERED BY ARIS187 ID*_
+╚════════════════════`
+}
